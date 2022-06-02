9b858012d19f1ca3f4a3e84abf7ac850a4009af9
andywtse <atse4995@gmail.com>
2022-06-02 15:49:52 -0400

    Adds game win condition and reset

--- js/app.js
+++ js/app.js
@@ -5,46 +5,54 @@ const arrayBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 /*---------------------------- Variables (state) ----------------------------*/
 
 let playerTurn = true // True === X && False === O
+let isGameStopped = false;
 
 /*------------------------ Cached Element References ------------------------*/
 
-const board = document.querySelectorAll('.square');
+const board = document.querySelector('.board');
+const squares = document.querySelectorAll('.square');
 const resetButton = document.getElementById('reset-button');
 const headerMessage = document.getElementById('message');
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-board.forEach(function (squares) {
-  squares.addEventListener('click', handleClick);
-});
+board.addEventListener("click", handleClick);
 resetButton.addEventListener('click', init);
 
 /*-------------------------------- Functions --------------------------------*/
 
+//Initializes the game with default values
 function init() {
+
+  // Re-assigns values so Player X turn is explicit
   playerTurn = true;
+  isGameStopped = false;
   headerMessage.innerText = "Player's X Turn";
 
-  arrayBoard.forEach(element => {
-    element = null;
-  });
-  board.forEach(square => {
+  // Re-assigns the array values to 0
+  for (let i = 0; i<arrayBoard.length; i++ ) {
+    arrayBoard[i] = 0;
+  } 
+
+  // Clears innerText of the squares
+  squares.forEach(square => {
     square.innerText = '';
   });
-  
-
-
 };
 
+// Handles click events
 function handleClick(evt) {
-
-  if (evt.target.innerText === '') {
-    render(evt);
-    playerTurn = !playerTurn; //Flip boolean
-  }
+  // If game is not stopped, allow process
+  if(!isGameStopped){
+    if (evt.target.innerText === '') {
+      render(evt);
+      playerTurn = !playerTurn; //Flip boolean
+    }
+  };
   
 };
 
+// Modifies most of the game visuals
 function render(evt) {
 
   if (playerTurn) {
@@ -57,15 +65,50 @@ function render(evt) {
     headerMessage.innerText = "Player's X Turn";
     let nodeIndex = parseNodeIndex(evt.target);
     arrayBoard[nodeIndex] = -1;
-  }
+  };
 
+  checkGameStatus();
+
+  //Check if a tie has occured
+  if (!isGameStopped) {
+    let isBoardFilled = arrayBoard.every( square => {
+      return square !== 0;
+    });
+    if(isBoardFilled) {
+      isGameStopped = true;
+      headerMessage.innerText = "Game ended in a tie!!!";
+    };
+  };
 };
 
+// Parses Square ID's for index
 function parseNodeIndex(element) {
-  //console.log(element.id.slice(-1))
   return parseInt(element.id.slice(-1));
 };
 
-function checkWinner() {
+// Checks if there is a winner and proceeds to end game if true
+function checkGameStatus() {
+  if (arrayBoard[0] + arrayBoard[1] + arrayBoard [2] === 3 ||
+      arrayBoard[3] + arrayBoard[4] + arrayBoard [5] === 3 ||
+      arrayBoard[6] + arrayBoard[7] + arrayBoard [8] === 3 ||
+      arrayBoard[0] + arrayBoard[3] + arrayBoard [6] === 3 ||
+      arrayBoard[1] + arrayBoard[4] + arrayBoard [7] === 3 ||
+      arrayBoard[2] + arrayBoard[4] + arrayBoard [8] === 3 ||
+      arrayBoard[0] + arrayBoard[4] + arrayBoard [8] === 3 ||
+      arrayBoard[2] + arrayBoard[4] + arrayBoard [6] === 3 ) {
+        isGameStopped = true;
+        headerMessage.innerText = "Player X is the Winner!!!";
+  }
 
+  if (arrayBoard[0] + arrayBoard[1] + arrayBoard [2] === -3 ||
+      arrayBoard[3] + arrayBoard[4] + arrayBoard [5] === -3 ||
+      arrayBoard[6] + arrayBoard[7] + arrayBoard [8] === -3 ||
+      arrayBoard[0] + arrayBoard[3] + arrayBoard [6] === -3 ||
+      arrayBoard[1] + arrayBoard[4] + arrayBoard [7] === -3 ||
+      arrayBoard[2] + arrayBoard[4] + arrayBoard [8] === -3 ||
+      arrayBoard[0] + arrayBoard[4] + arrayBoard [8] === -3 ||
+      arrayBoard[2] + arrayBoard[4] + arrayBoard [6] === -3 ) {
+        isGameStopped = true; 
+        headerMessage.innerText = "Player O is the Winner!!!";
+  };
 };
