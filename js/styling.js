7b837513063279d592f83a23dd7ae24afec8002e
andywtse <atse4995@gmail.com>
2022-06-03 09:20:06 -0400

    Changes objectFill for body

--- js/styling.js
+++ js/styling.js
@@ -1,4 +1,5 @@
 let bodyElement = document.querySelector('body');
 bodyElement.style.backgroundImage="url(resource/img/green.jpeg)";
-bodyElement.style.backgroundSize = "cover";
+// bodyElement.style.backgroundSize = "cover";
+bodyElement.style.objectFit = "fill"
 bodyElement.style.backgroundRepeat = "no-repeat";
